4d6da4adebfb250dab1adc0f02bfb5bb3e8d6cdb
Atmanand Gauns <sanketgauns8@gmail.com>
2024-12-08 22:21:26 +0530

    feat : Big optimization and max line option

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -11,98 +11,127 @@
   import { afterUpdate, createEventDispatcher, onMount } from "svelte";
   import { skipNextTransition, app, view, settings } from "./store";
   import { TitleDisplayMode } from "../settings";
-  import type { Child } from "svelte-eslint-parser/lib/parser/compat";
 
   export let file: TFile;
-  let displayFilename: boolean = true;
+  let displayFilename: boolean =
+    $settings.displayTitle !== TitleDisplayMode.Title;
   let contentDiv: HTMLElement;
   let pinned: boolean;
   $: pinned = $settings.pinnedFiles.includes(file.path);
 
-  function postProcessor(
-    element: HTMLElement,
-    context: MarkdownPostProcessorContext,
-  ) {
-    if (context.sourcePath !== file.path) {
-      // Very important to check if the sourcePath is the same as the file path
-      // Otherwise, the post processor will be applied to all files
-      return;
-    }
+  // function postProcessor(
+  //   element: HTMLElement,
+  //   context: MarkdownPostProcessorContext,
+  // ) {
+  //   if (context.sourcePath !== file.path) {
+  //     // Very important to check if the sourcePath is the same as the file path
+  //     // Otherwise, the post processor will be applied to all files
+  //     return;
+  //   }
 
-    if (
-      $settings.displayTitle == TitleDisplayMode.Filename &&
-      element.children.length > 0 &&
-      element.children[0].tagName === "H1" &&
-      element.children[0].textContent
-    ) {
-      element.children[0].remove();
-    } else if ($settings.displayTitle == TitleDisplayMode.Title) {
-      displayFilename = false;
-    }
+  //   if (
+  //     $settings.displayTitle == TitleDisplayMode.Filename &&
+  //     element.children.length > 0 &&
+  //     element.children[0].tagName === "H1" &&
+  //     element.children[0].textContent
+  //   ) {
+  //     element.children[0].remove();
+  //   } else if ($settings.displayTitle == TitleDisplayMode.Title) {
+  //     displayFilename = false;
+  //   }
 
-    if (element.children.length === 0) {
-      return;
-    }
+  //   if (element.children.length === 0) {
+  //     return;
+  //   }
 
-    // Add shadow to blocks of embed notes (we take all .internal-embed,
-    // including images, because we can't differentiate them at that time,
-    // but CSS will only apply to embed notes)
-    for (let i = 0; i < element.children.length; i++) {
-      if (
-        element.children[i].getElementsByClassName("internal-embed").length ||
-        element.children[i].className.includes("block-language-dataview")
-      ) {
-        // console.log("Adding shadow to embed note");
-        element.children[i].appendChild(
-          document.createElement("div"),
-        ).className = "embed-shadow";
-      }
-    }
+  //   // Add shadow to blocks of embed notes (we take all .internal-embed,
+  //   // including images, because we can't differentiate them at that time,
+  //   // but CSS will only apply to embed notes)
+  //   for (let i = 0; i < element.children.length; i++) {
+  //     if (
+  //       element.children[i].getElementsByClassName("internal-embed").length ||
+  //       element.children[i].className.includes("block-language-dataview")
+  //     ) {
+  //       // console.log("Adding shadow to embed note");
+  //       element.children[i].appendChild(
+  //         document.createElement("div"),
+  //       ).className = "embed-shadow";
+  //     }
+  //   }
 
-    // Find block where to cut the preview
-    let lastBlockIndex: number = 0,
-      charCount: number = 0;
-    do {
-      charCount += element.children[lastBlockIndex]?.textContent?.length || 0;
-    } while (
-      lastBlockIndex < element.children.length &&
-      charCount < 200 &&
-      ++lastBlockIndex
-    );
+  //   // Find block where to cut the preview
+  //   let lastBlockIndex: number = 0,
+  //     charCount: number = 0;
+  //   do {
+  //     charCount += element.children[lastBlockIndex]?.textContent?.length || 0;
+  //   } while (
+  //     lastBlockIndex < element.children.length &&
+  //     charCount < 200 &&
+  //     ++lastBlockIndex
+  //   );
 
-    // Remove all blocks after the last block
-    for (let i = element.children.length - 1; i > lastBlockIndex; i--) {
-      element.children[i]?.remove();
-    }
+  //   // Remove all blocks after the last block
+  //   for (let i = element.children.length - 1; i > lastBlockIndex; i--) {
+  //     element.children[i]?.remove();
+  //   }
 
-    if (charCount < 200) {
-      return;
-    }
+  //   if (charCount < 200) {
+  //     return;
+  //   }
 
-    // Cut the last block
-    if (
-      !element.children[lastBlockIndex].lastChild ||
-      element.children[lastBlockIndex].lastChild?.nodeType !== Node.TEXT_NODE
-    ) {
-      return;
-    }
+  //   // Cut the last block
+  //   if (
+  //     !element.children[lastBlockIndex].lastChild ||
+  //     element.children[lastBlockIndex].lastChild?.nodeType !== Node.TEXT_NODE
+  //   ) {
+  //     return;
+  //   }
 
-    // TODO : I have to find out how many words will fit in the card, when a max-hight option has been set, to add the '...' at the end.
-    const lastElText = element.children[lastBlockIndex].lastChild?.textContent;
-    if (lastElText != null) {
-      const cut = Math.min(50, 200 - (charCount - lastElText.length));
-      (element.children[lastBlockIndex].lastChild as Child).textContent =
-        `${lastElText.slice(0, cut)} ...`;
-    }
-  }
+  //   // TODO : I have to find out how many words will fit in the card, when a max-hight option has been set, to add the '...' at the end.
+  //   const lastElText = element.children[lastBlockIndex].lastChild?.textContent;
+  //   if (lastElText != null) {
+  //     const cut = Math.min(50, 200 - (charCount - lastElText.length));
+  //     (element.children[lastBlockIndex].lastChild as Child).textContent =
+  //       `${lastElText.slice(0, cut)} ...`;
+  //   }
+  // }
+
+  // const renderFile = async (el: HTMLElement): Promise<void> => {
+  //   const content = await file.vault.cachedRead(file);
+  //   if (content.trim().length > 0) {
+  //     MarkdownPreviewRenderer.registerPostProcessor(postProcessor);
+  //     await MarkdownRenderer.render($app, content, el, file.path, $view);
+  //     MarkdownPreviewRenderer.unregisterPostProcessor(postProcessor);
+  //   } else {
+  //     el.createEl("div", {
+  //       text: "File is Empty",
+  //       cls: "card-content-empty",
+  //     });
+  //   }
+  // };
+
+  // Function to truncate content to a specific number of words
+  const truncateContent = (content: string, maxLiness: number): string => {
+    const lines = content.split(/\r?\n/);
+    return lines.length > maxLiness
+      ? lines.slice(0, maxLiness).join("\n")
+      : content;
+  };
 
   const renderFile = async (el: HTMLElement): Promise<void> => {
     const content = await file.vault.cachedRead(file);
-    // TODO : Need to add the logic to detect frontmatter and show it.
     if (content.trim().length > 0) {
-      MarkdownPreviewRenderer.registerPostProcessor(postProcessor);
-      await MarkdownRenderer.render($app, content, el, file.path, $view);
-      MarkdownPreviewRenderer.unregisterPostProcessor(postProcessor);
+      const maxLiness = $settings.maxLines || 20;
+      const truncatedContent = truncateContent(content, maxLiness);
+
+      await MarkdownRenderer.render(
+        $app,
+        truncatedContent,
+        el,
+        file.path,
+        $view,
+      );
+      postProcessRenderedContent(el);
     } else {
       el.createEl("div", {
         text: "File is Empty",
@@ -110,6 +139,54 @@
       });
     }
   };
+
+  // Post-process rendered content for optimizations
+  const postProcessRenderedContent = (element: HTMLElement) => {
+    if ($settings.displayTitle === TitleDisplayMode.Filename) {
+      const firstChild = element.firstElementChild;
+      if (firstChild?.tagName === "H1") {
+        firstChild.remove();
+      }
+    }
+
+    // Add shadows to embeds and dataview blocks
+    element
+      .querySelectorAll(".internal-embed, .block-language-dataview")
+      .forEach((embed) => {
+        embed.appendChild(document.createElement("div")).className =
+          "embed-shadow";
+      });
+
+    // Truncate the preview if needed
+    // let charCount = 0;
+    // Array.from(element.children).forEach((child, index) => {
+    //   charCount += child.textContent?.length || 0;
+    //   if (charCount > 200) {
+    //     truncateLastBlock(child, charCount, 200);
+    //     // Remove extra siblings
+    //     while (child.nextSibling) {
+    //       child.nextSibling.remove();
+    //     }
+    //   }
+    // });
+  };
+
+  // // Truncate the last block to fit within the character limit
+  // const truncateLastBlock = (
+  //   block: Element,
+  //   charCount: number,
+  //   maxChars: number,
+  // ) => {
+  //   const textNode = Array.from(block.childNodes).find(
+  //     (node) => node.nodeType === Node.TEXT_NODE,
+  //   ) as Text | null;
+
+  //   if (textNode) {
+  //     const excessChars = charCount - maxChars;
+  //     const visibleText = textNode.textContent!.slice(0, -excessChars) + " ...";
+  //     textNode.textContent = visibleText;
+  //   }
+  // };
 
   const togglePin = async () => {
     $settings.pinnedFiles = pinned
@@ -251,39 +328,39 @@
   ></div>
 
   <div class="card-info-parent">
-  <div class="card-info">
-    {#if pinned}
-      <button
-        class="clickable-icon"
-        use:pinnedIcon
-        on:click|stopPropagation={togglePin}
-      />
-    {:else}
-      <button
-        class="clickable-icon"
-        class:is-active={pinned}
-        use:pinButton
-        on:click|stopPropagation={togglePin}
-      />
-    {/if}
-    {#if file.parent != null && file.parent.path !== "/"}
-      <div class={folderIconClass}>
-        <span use:folderIcon />{file.parent.path}
-      </div>
-    {:else}
-      <div class={folderIconClass}>
-        <span use:vaultIcon />Root
-      </div>
-    {/if}
-    {#if $settings.showDeleteButton}
-      <button
-        class="clickable-icon"
-        use:trashIcon
-        on:click|stopPropagation={trashFile}
-      />
-    {:else}
-      <button class="clickable-icon" use:blankIcon />
-    {/if}
+    <div class="card-info">
+      {#if pinned}
+        <button
+          class="clickable-icon"
+          use:pinnedIcon
+          on:click|stopPropagation={togglePin}
+        />
+      {:else}
+        <button
+          class="clickable-icon"
+          class:is-active={pinned}
+          use:pinButton
+          on:click|stopPropagation={togglePin}
+        />
+      {/if}
+      {#if file.parent != null && file.parent.path !== "/"}
+        <div class={folderIconClass}>
+          <span use:folderIcon />{file.parent.path}
+        </div>
+      {:else}
+        <div class={folderIconClass}>
+          <span use:vaultIcon />Root
+        </div>
+      {/if}
+      {#if $settings.showDeleteButton}
+        <button
+          class="clickable-icon"
+          use:trashIcon
+          on:click|stopPropagation={trashFile}
+        />
+      {:else}
+        <button class="clickable-icon" use:blankIcon />
+      {/if}
     </div>
   </div>
 </div>
