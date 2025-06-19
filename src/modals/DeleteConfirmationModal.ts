ebb66280b5baccd19bbfd34958399d42fe43a3e8
Atmanand Gauns <sanketgauns8@gmail.com>
2025-06-19 10:29:36 +0530

    add back the `modals` directory

---
+++ src/modals/DeleteConfirmationModal.ts
@@ -0,0 +1,80 @@
+// src/Modal/DeleteConfirmationModal.tsx
+
+import { App, Modal } from "obsidian";
+
+import { DeleteFileMode } from "src/settings";
+import { get } from "svelte/store";
+import { settings } from "src/components/store";
+
+interface DeleteConfirmationModalProps {
+  app: App; // Add this field
+  mssg: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+export default class DeleteConfirmationModal extends Modal {
+  mssg: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+
+  constructor({
+    app,
+    mssg,
+    onConfirm,
+    onCancel,
+  }: DeleteConfirmationModalProps) {
+    super(app);
+    this.mssg = mssg;
+    this.onConfirm = onConfirm;
+    this.onCancel = onCancel;
+  }
+
+  onOpen() {
+    const { contentEl } = this;
+
+    this.modalEl.setAttribute("data-type", "notes-explorer");
+
+    let trashLocMssg = "";
+    if (get(settings).deleteFileMode === DeleteFileMode.System) {
+      trashLocMssg =
+        "The note file will be moved in the system trash. You can easily restore it later if required.";
+    } else if (get(settings).deleteFileMode === DeleteFileMode.Trash) {
+      trashLocMssg =
+        "The note file will be moved in the vault's trash folder. You can easily restore it later from the .trash folder in your vault folder.";
+    } else {
+      trashLocMssg =
+        "Even though the delete file mode is set as permanent. Still to avoid any accidental data loss, the file will be moved to system's trash. You can delete it from there permanently.";
+    }
+    const homeComponent = contentEl.createEl("span", {
+      cls: "deleteConfirmationModalHome",
+    });
+    homeComponent.createEl("h2", { text: this.mssg });
+    homeComponent.createEl("p", { text: trashLocMssg });
+
+    const buttonContainer = homeComponent.createDiv(
+      "deleteConfirmationModalHome-button-container"
+    );
+
+    const confirmButton = buttonContainer.createEl("button", {
+      text: "Delete",
+    });
+    confirmButton.classList.add("deleteTaskConfirmBtn");
+    confirmButton.addEventListener("click", () => {
+      this.onConfirm();
+      this.close();
+    });
+
+    const cancelButton = buttonContainer.createEl("button", { text: "Cancel" });
+    cancelButton.classList.add("deleteTaskCancelmBtn");
+    cancelButton.addEventListener("click", () => {
+      this.onCancel();
+      this.close();
+    });
+  }
+
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
+}
---
+++ src/modals/NotesCountStatisticsModal.ts
@@ -0,0 +1,69 @@
+// /src/modal/NotesCountStatisticsModal.ts
+
+import { App, Modal } from "obsidian";
+import {
+  allAllowedFiles,
+  excludedFilesCount,
+  filteredFiles,
+} from "src/components/store";
+
+import type NotesExplorerPlugin from "main";
+import { get } from "svelte/store";
+
+export class NotesCountStatisticsModal extends Modal {
+  constructor(plugin: NotesExplorerPlugin) {
+    super(plugin.app);
+  }
+
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.empty();
+
+    const allNotesInVaultCount = this.app.vault.getMarkdownFiles().length;
+
+    const title = contentEl.createEl("h2", { text: "Notes Statistics" });
+    title.addClass("notes-explorer-modal-title");
+    const totalFilesEl = contentEl.createDiv({
+      cls: "notes-explorer-modal-text-element",
+    });
+    totalFilesEl.createEl("p", {
+      text: "Total notes in vault: ",
+    });
+    totalFilesEl.createEl("p", {
+      text: `${allNotesInVaultCount}`,
+      cls: "notes-explorer-modal-text-element-highlight",
+    });
+
+    const allAllowedFilesEl = contentEl.createDiv({
+      cls: "notes-explorer-modal-text-element",
+    });
+    allAllowedFilesEl.createEl("p", {
+      text: "Total notes allowed for Notes Explorer to fetch: ",
+    });
+    allAllowedFilesEl.createEl("p", {
+      text: `${allNotesInVaultCount - get(excludedFilesCount)}`,
+      cls: "notes-explorer-modal-text-element-highlight",
+    });
+
+    const filteredFilesEl = contentEl.createDiv({
+      cls: "notes-explorer-modal-text-element",
+    });
+    filteredFilesEl.createEl("p", {
+      text: "Total filtered notes from the folder-tag, search-query or search-filters: ",
+    });
+    filteredFilesEl.createEl("p", {
+      text: `${get(filteredFiles).length}`,
+      cls: "notes-explorer-modal-text-element-highlight",
+    });
+
+    // contentEl.appendChild(title);
+    // contentEl.appendChild(totalFilesEl);
+    // contentEl.appendChild(allAllowedFilesEl);
+    // contentEl.appendChild(filteredFilesEl);
+  }
+
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
+}
